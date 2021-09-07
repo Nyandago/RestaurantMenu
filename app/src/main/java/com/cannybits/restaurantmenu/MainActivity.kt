4b150ee395e3f6787b83e0c9ac4dae846049da1f
nyandago <paulmutator@gmail.com>
2021-09-08 00:04:48 +0300

    Added Food elements

--- app/src/main/java/com/cannybits/restaurantmenu/MainActivity.kt
+++ app/src/main/java/com/cannybits/restaurantmenu/MainActivity.kt
@@ -4,8 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var foodList = ArrayList<Food>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        foodList.add(Food("Coffee","Kahawa ya Arabica inapatikana",R.drawable.coffee_pot))
+        foodList.add(Food("Espresso","Espresso inapatikana pia",R.drawable.espresso))
+        foodList.add(Food("French Fries","chips kavu",R.drawable.french_fries))
+        foodList.add(Food("Honey","Asali tamu kutoka Tabora",R.drawable.honey))
+        foodList.add(Food("Strawberry Ice Cream","Ice cream ya matunda ya straw berry",R.drawable.strawberry_ice_cream))
+        foodList.add(Food("Sugar Cubes","Sukari guru ile yenyewe kabisa",R.drawable.sugar_cubes))
     }
 }
