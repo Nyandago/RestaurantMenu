92eb7f359417c47931e094cfdd74ec4a6c220de9
nyandago <paulmutator@gmail.com>
2021-09-08 00:47:43 +0300

    Food adapter

--- app/src/main/java/com/cannybits/restaurantmenu/MainActivity.kt
+++ app/src/main/java/com/cannybits/restaurantmenu/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.cannybits.restaurantmenu
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 
 class MainActivity : AppCompatActivity() {
 
     var foodList = ArrayList<Food>()
+    var adapter: FoodAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +23,34 @@ class MainActivity : AppCompatActivity() {
         foodList.add(Food("Honey","Asali tamu kutoka Tabora",R.drawable.honey))
         foodList.add(Food("Strawberry Ice Cream","Ice cream ya matunda ya straw berry",R.drawable.strawberry_ice_cream))
         foodList.add(Food("Sugar Cubes","Sukari guru ile yenyewe kabisa",R.drawable.sugar_cubes))
+        adapter = FoodAdapter(this, foodList)
+    }
+
+    class FoodAdapter : BaseAdapter{
+        var foodList = ArrayList<Food>()
+        var context: Context? = null
+
+        constructor(context: Context, foodList:ArrayList<Food>):super(){
+            this.context = context
+            this.foodList = foodList
+        }
+
+        override fun getCount(): Int {
+            return foodList.size
+        }
+
+        override fun getItem(position: Int): Any {
+           return foodList[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+           return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+         var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var foodView = inflater.inflate(R.layout.food_card,null)
+            foodView.iv
+        }
     }
 }
